a890ea431c0d23d023da1a3f4bd7f94a71bd215b
Yanetta <revenkot81@gmail.com>
2019-01-01 21:11:16 +0200

    Revert "3 commit on branch2"
    This reverts commit 617f2be

--- src/main/java/bankWithGenerics/Bank.java
+++ src/main/java/bankWithGenerics/Bank.java
@@ -1,31 +1,4 @@
 package bankWithGenerics;
 
-public class Bank <T extends Asset> {
-    String bankName;
-    String adress;
-    T active;
-
-    public Bank(String name, String adress, T active) {
-        this.bankName = name;
-        this.adress = adress;
-        this.active = active;
-    }
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-    public String getAdress() {
-        return adress;
-    }
-    public void setName(String name) {
-        this.bankName = name;
-    }
-    public String getName() {
-        return bankName;
-    }
-    public void setActive(T active) {
-        this.active = active;
-    }
-    public T getActive() {
-        return active;
-    }
+public class Bank {
 }
