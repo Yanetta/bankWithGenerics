e1f72ec01fe91eb861fd86456dab04004ba402d6
Yanetta <revenkot81@gmail.com>
2019-01-01 21:04:06 +0200

    4 commit on branch2

--- src/main/java/bankWithGenerics/Securities.java
+++ src/main/java/bankWithGenerics/Securities.java
@@ -3,9 +3,12 @@ package bankWithGenerics;
 public class Securities extends BankInstrument implements Asset {
     public Securities(int amount) {
         super(amount);
+        nameOfInstrument = "Securities";
+
     }
 
     public String trendOnMarket() {
-        return null;
+        String s = " Price of securities is falling";
+        return s;
     }
 }
